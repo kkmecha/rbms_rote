4578354232755a604a4d48637e207a5de39717f7
kkmecha <167722055+kkmecha@users.noreply.github.com>
2024-09-27 16:25:05 +0900

    Add files via upload

---
+++ rbms_rote.cpp
@@ -0,0 +1,182 @@
+#include "rbms_rote.h"
+
+rbms_rote::rbms_rote(CAN &can, bool motor_type, int motor_num)
+            : _can(can), _motor_type(motor_type), _motor_num(motor_num){
+    if(_motor_type){
+        _motor_max = TORQUE_MAX_M3508;
+    }else{
+        _motor_max = TORQUE_MAX_M2006;
+    }
+    if(_motor_num <= 8){
+        _can.frequency(1000000);
+        _can.mode(CAN::Normal);
+    }
+}
+
+int rbms_rote::rbms_send(int* motor) {
+    char _byte[_motor_num*2];
+    int _a = 0;
+    for(int i = 0; i < _motor_num; i++){ 
+        if(motor[i] > _motor_max)
+            return 0;
+        _byte[_a++] = (char)(motor[i] >> 8);
+        _byte[_a++] = (char)(motor[i] & 0xFF); 
+    }
+
+    _canMessage.id = 0x200;//esc id1~4のcanの送信id
+    _canMessage.len = 8;//can data長(8byte固定)
+    _canMessage2.id = 0x1ff;//esc id5~8のcanの送信id
+    _canMessage2.len = 8;
+    _a = 0;
+    int _i = 0;
+    for(int i = 0; i < _motor_num; i++){
+        if(i < 4){
+            _canMessage.data[_a] = _byte[_a];
+            _a++;
+            _canMessage.data[_a] = _byte[_a];
+            _a++;
+        }else{
+            _canMessage2.data[_i++] = _byte[_a++];
+            _canMessage2.data[_i++] = _byte[_a++];
+        }
+    }
+    while(_a < 15){
+        if(_a < 7){
+            _canMessage.data[_a++] = 0;
+            _canMessage.data[_a++] = 0;
+        }else{
+            _canMessage2.data[_a++] = 0;
+            _canMessage2.data[_a++] = 0;
+        } 
+    }
+   
+    if (_can.write(_canMessage)&&_can.write(_canMessage2)) {
+        return 1;
+    }else{
+        return -1;
+    }
+
+}
+
+void rbms_rote::rbms_read(CANMessage &msg, short *rotation, short *speed) {
+            _r = (msg.data[0] << 8) | (msg.data[1] & 0xff);
+            _rotation = (float)_r / 8192 * 360;//8192=360°
+            *rotation = _rotation;
+ 
+            _speed = (msg.data[2] << 8) | (msg.data[3] & 0xff);
+            if (_speed & 0b1000000000000000){//マイナス値の場合(最上位ビットが1のとき)(2の補数)
+                _speed--;
+                _speed = -~_speed;
+            }
+            *speed = _speed;
+
+            _torque = (msg.data[4] << 8) | (msg.data[5] & 0xff);
+            if (_torque & 0b1000000000000000){
+                _torque--;
+                _torque = -~_torque;
+            }
+
+            _temperature = msg.data[6];
+            
+}
+
+void rbms_rote::rote_robo_ms_update(CANMessage *msg, int BUFFER_MAX, bool angle_type)
+{
+    _angle_type = angle_type;
+    // if(_debugmsg)printf("motor%d sumS:%d sumD:%d rote:%d spd:%d deltaR:%d tmpR:%d\n",_motornum,(int)sumRstatic,(int)sumRdynamic,rote,spd,deltaR,tmpR);
+    int msgnum = 0x201 + _motor_num;
+    for (int i = 0; i < BUFFER_MAX; i++)
+    {
+        if (msg[i].id == msgnum)
+            _msg = msg[i];
+    }
+    rbms_read(_msg, &rote, &spd);
+    
+    if (rote < tmpR && spd > 0) // 正転時
+    {
+        deltaR = (short)(360 - tmpR) + rote;
+    }
+    else if (rote > tmpR && spd > 0)
+    {
+        deltaR = rote - tmpR;
+    }
+    if (rote > tmpR && spd < 0) // 反転時    
+    {
+        deltaR = (short)(360 - rote) + tmpR;
+        deltaR *= -1;
+    }
+    else if (rote < tmpR && spd < 0)
+    {
+        deltaR = rote - tmpR;
+    }
+    if (rote == tmpR) //変化なし
+    {
+        deltaR = 0;
+    }
+
+    // printf("%x\n",msgnum);
+    // printf("motor%d length:%d spd:%d rote:%d deltaR:%d\n",_motornum,(int)sumLength,spd,(int)sumRdynamic,deltaR);
+
+    sumRstatic += deltaR;
+
+    tmpR = rote;
+}
+
+float rbms_rote::speed_pid(float T, short rpm_now, short set_speed,float *delta_rpm_pre,float *ie,float KP, float KI,float KD )//pid制御
+{
+    float de;
+    float delta_rpm;
+    delta_rpm  = set_speed - rpm_now;
+    de = (delta_rpm - *delta_rpm_pre)/T;
+    *ie = *ie + (delta_rpm + *delta_rpm_pre)*T/2;
+    float out_torque  = KP*delta_rpm + KI*(*ie) + KD*de;
+    *delta_rpm_pre = delta_rpm;
+    return out_torque;
+}
+
+void rbms_rote::spd_control(int* set_speed,int* motor){
+    short rotation[_motor_num], speed[_motor_num];
+    float delta_rpm_pre[_motor_num], ie[_motor_num];
+    Timer tm[_motor_num];
+    for(int i=0;i<_motor_num;i++){//初期化
+        delta_rpm_pre[i]=0.0;
+        ie[i]=0.0;
+        tm[i].start();
+    }
+    
+    while(1){
+        for(int id=0;id<_motor_num;id++){
+            if(_msg.id==0x201+id){
+                CANMessage msg=_msg;
+                rbms_read(msg,&rotation[id],&speed[id]);
+                if(_motor_type){
+                    motor[id] = (int)speed_pid(chrono::duration_cast<chrono::milliseconds>(tm[id].elapsed_time()).count(), speed[id]/GIAR_RATIO_M3508, set_speed[id], &delta_rpm_pre[id], &ie[id], KP, KI, KD);
+                }else{
+                    motor[id] = (int)speed_pid(chrono::duration_cast<chrono::milliseconds>(tm[id].elapsed_time()).count(), speed[id]/GIAR_RATIO_M2006, set_speed[id], &delta_rpm_pre[id], &ie[id], KP, KI, KD);
+                }
+                tm[id].reset();
+                if(motor[id] > _motor_max){
+                    motor[id] = _motor_max;
+                }else if(motor[id] < -_motor_max){
+                    motor[id] = -_motor_max;
+                }
+            }
+        }
+        ThisThread::sleep_for(3ms);
+    }
+}
+
+long rbms_rote::get_rote()
+{
+    if(_angle_type)
+        return sumRstatic;
+    else
+        return static_cast<long>(rote);
+}
+
+void rbms_rote::set_static_reset(int num)
+{
+    sumRstatic = num;
+}
+
+
---
+++ rbms_rote.h
@@ -0,0 +1,50 @@
+#ifndef __RBMS_ROTE_H__
+#define __RBMS_ROTE_H__
+
+#define TORQUE_MAX_M2006 10000
+#define TORQUE_MAX_M3508 16384
+#define GIAR_RATIO_M2006 36
+#define GIAR_RATIO_M3508 19
+#define DEG_PER_RAD 0.017444f
+#define BRAKE_GAIN 4
+
+#include "mbed.h"
+
+class rbms_rote{
+    public:
+        rbms_rote(CAN &can, bool motor_type, int motor_num);
+        int rbms_send(int* motor);
+        void rote_robo_ms_update(CANMessage *msg, int BUFFER_MAX, bool angle_type);
+        void spd_control(int* set_speed,int* motor); 
+        void reset_ie();
+        long get_rote();
+        void set_static_reset(int num);
+        float KP = 25;
+        float KI = 10;
+        float KD = 0;
+
+    private:
+        void rbms_read(CANMessage &msg, short *rotation, short *speed);
+        float speed_pid(float T,short rpm_now, short set_speed,float *delta_rpm_pre,float *ie,float KP,float KI, float KD);
+        CANMessage _canMessage,_canMessage2,_msg;
+        CAN &_can;
+        bool _motor_type;//if 0 m2006,if 1 m3508
+        int _motor_num,_motor_max;
+        unsigned short _r;
+        int _rotation, _speed, _torque, _temperature;
+        bool _angle_type;
+
+        short deltaR, tmpR, rote, spd; // 角度計算用なので別にしている
+        long sumRstatic;
+};
+
+#endif
+
+
+
+
+
+
+
+
+
